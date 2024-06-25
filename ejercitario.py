f407ffa869cd8d5debc6763955f5517f39278b22
Manuel Rene Pauls Toews <rene.pauls@avantys.de>
2024-06-25 14:55:47 -0400

    Agregar ejercicio 2

--- ejercitario.py
+++ ejercitario.py
@@ -36,13 +36,34 @@ def ej1(img):
 
     return cv2.bitwise_not(lines)
 
+def ej2(img):
+    img = cv2.bitwise_not(img)
+    # Creamos un kernel circular, porque va afectar menos a circuos en erosion
+    kernel_circ = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5,5))
+
+    # Erosionamos 8 veces, como ya mencionado la forma del kernel afecta más a rectangulos que a circulos
+    erode = cv2.erode(img, kernel_circ, iterations=8)
+
+    # Descomentar para debug
+    # cv2.imshow('erode', erode)
+    # cv2.waitKey(0)
+
+    reconstruction_img = reconstruction(img, erode)
+    return cv2.bitwise_not(reconstruction_img)
 
 # main
 image = cv2.imread('binbolin.jpg', cv2.IMREAD_GRAYSCALE)
-neg = cv2.bitwise_not(image)
 
 r1 = ej1(image)
 cv2.imshow('image', image)
 cv2.imshow('r1', r1)
 cv2.waitKey(0)
+cv2.destroyAllWindows()
 
+image = cv2.imread('binquad4.bmp', cv2.IMREAD_GRAYSCALE)
+r2 = ej2(image)
+
+cv2.imshow('image', image)
+cv2.imshow('r2', r2)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
